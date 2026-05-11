6ec2c3973e946537d2c1c766c970dfc589a0608d
habel <75840991+habel5@users.noreply.github.com>
2026-05-11 10:49:23 +0200

    Update Supabase configuration with new URL and anonKey

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://bgredggvmseuoyrunfha.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJncmVkZ2d2bXNldW95cnVuZmhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0Nzk3MzQsImV4cCI6MjA5NDA1NTczNH0.5ipUKSRgerlaK0xABKgTLnnDvFAAVOB_kw1Ksg56mYE",
   bucket: "bewijslast"
 };
